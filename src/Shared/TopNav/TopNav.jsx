679b4c56d27bdb67fe3eb3f812406845771478ec
alaminwbsoft <wbsoft.alamin@gmail.com>
2024-11-12 10:54:23 +0600

    add

---
+++ src/Shared/TopNav/TopNav.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import topBannerImg from '../../assets/topBannerImg.gif'
+const TopNav = () => {
+    return (
+        <div>
+            <img src={topBannerImg} alt="" />
+        </div>
+    );
+};
+
+export default TopNav;
