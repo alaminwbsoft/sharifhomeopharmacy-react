09bb98268c4103f0f4b78e17d9c19e4fc19bcfbb
sabrinaWorkK <wbsoft.sabrina@gmail.com>
2024-11-12 11:21:12 +0600

    add

--- src/Router/Router.jsx
+++ src/Router/Router.jsx
@@ -2,18 +2,23 @@ import { createBrowserRouter } from "react-router-dom";
 import LayOut from "../LayOut/LayOut";
 import ErrorPage from "../ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home";
+import DilutionsAndPotency from "../Pages/Medicine-Category-Dilutions-and-Potencies/DilutionsAndPotency";
 
 const Router = createBrowserRouter([
-    {
+  {
+    path: "/",
+    element: <LayOut />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
         path: "/",
-        element: <LayOut/>,
-        errorElement:<ErrorPage/>,
-        children: [
-          {
-            path: "/",
-            element: <Home/>
-          }
-        ],
-    }
-])
+        element: <Home />,
+      },
+      {
+        path: "/dilutions",
+        element: <DilutionsAndPotency />,
+      },
+    ],
+  },
+]);
 export default Router;
