e932c43b034699e114e2be79e32a7c96ca6921a5
alaminwbsoft <wbsoft.alamin@gmail.com>
2024-11-13 22:40:10 +0600

    add

--- src/LayOut/LayOut.jsx
+++ src/LayOut/LayOut.jsx
@@ -1,20 +1,17 @@
-
-import { Outlet } from 'react-router-dom';
-import Footer from '../Shared/Footer/Footer';
-import Header from '../Shared/Header/Header';
+import { Outlet } from "react-router-dom";
+import Footer from "../Shared/Footer/Footer";
+import Header from "../Shared/Header/Header";
 
 const LayOut = () => {
-    return (
-        <div>
-           
-            <div className='w-[90%] md:w-[95%] xl:w-[80%]  mx-auto pt-[74px] md:pt-[68px] lg:py-4 '>
-            <Header/>
-            <Outlet />
-            </div>
-            <Footer/>
-    
-        </div>
-    );
+  return (
+    <div>
+      <Header />
+      <div className="w-[90%] md:w-[95%] xl:w-[80%]  mx-auto pt-[74px] md:pt-[68px] lg:py-4 ">
+        <Outlet />
+      </div>
+      <Footer />
+    </div>
+  );
 };
 
 export default LayOut;
